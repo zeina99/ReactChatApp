6785c89f27dc4e7a0520aa28775765d83bc471f6
Zeina <51930164+zeina99@users.noreply.github.com>
2021-07-27 18:03:57 +0400

    send message by pressing enter instead of only having the option to click send button

--- client/src/components/ChatInput/ChatInput.js
+++ client/src/components/ChatInput/ChatInput.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import "./ChatInput.css";
 
 const ChatInput = ({ userInput, setUserInput, sendMessage }) => {
   const handleInput = (event) => {
     setUserInput(event.target.value);
   };
+  useEffect(() => {
+    function sendMessageOnClick(event) {
+      if (event.code === "Enter") {
+        sendMessage();
+        event.preventDefault();
+      }
+    }
+
+    document.addEventListener("keydown", sendMessageOnClick);
+
+    return () => {
+      document.removeEventListener("keydown", sendMessageOnClick);
+    };
+  }, [sendMessage]);
 
   return (
     <div className="input-area">
